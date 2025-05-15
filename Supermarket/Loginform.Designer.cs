9b1f672373ba21601bcd6b633448cf33dcd5d5ff
jisan <jisan@DESKTOP-JKM34KI>
2025-05-15 18:09:09 +0600

    Add project files.

---
+++ Supermarket/Loginform.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace Supermarket
+{
+	partial class Loginform
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.guna2CircleButton1 = new Guna.UI2.WinForms.Guna2CircleButton();
+			this.Label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.Text_Box_username = new Guna.UI2.WinForms.Guna2TextBox();
+			this.guna2TextBox2 = new Guna.UI2.WinForms.Guna2TextBox();
+			this.Button_login = new Guna.UI2.WinForms.Guna2Button();
+			this.label_clear = new Guna.UI2.WinForms.Guna2Button();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label_minimize = new System.Windows.Forms.Label();
+			this.label_exit = new System.Windows.Forms.Label();
+			this.comboBox_role = new System.Windows.Forms.ComboBox();
+			this.SuspendLayout();
+			// 
+			// guna2CircleButton1
+			// 
+			this.guna2CircleButton1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+			this.guna2CircleButton1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+			this.guna2CircleButton1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+			this.guna2CircleButton1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+			this.guna2CircleButton1.FillColor = System.Drawing.Color.Turquoise;
+			this.guna2CircleButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+			this.guna2CircleButton1.ForeColor = System.Drawing.Color.White;
+			this.guna2CircleButton1.Location = new System.Drawing.Point(-132, -17);
+			this.guna2CircleButton1.Name = "guna2CircleButton1";
+			this.guna2CircleButton1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+			this.guna2CircleButton1.Size = new System.Drawing.Size(415, 447);
+			this.guna2CircleButton1.TabIndex = 0;
+			this.guna2CircleButton1.Click += new System.EventHandler(this.guna2CircleButton1_Click);
+			// 
+			// Label1
+			// 
+			this.Label1.AutoSize = true;
+			this.Label1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Label1.Location = new System.Drawing.Point(304, 177);
+			this.Label1.Name = "Label1";
+			this.Label1.Size = new System.Drawing.Size(91, 20);
+			this.Label1.TabIndex = 1;
+			this.Label1.Text = "USERNAME";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(304, 234);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(91, 20);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "PASSWORD";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(471, 81);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(89, 37);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "Login";
+			this.label3.Click += new System.EventHandler(this.label3_Click);
+			// 
+			// Text_Box_username
+			// 
+			this.Text_Box_username.BorderRadius = 18;
+			this.Text_Box_username.Cursor = System.Windows.Forms.Cursors.IBeam;
+			this.Text_Box_username.DefaultText = "";
+			this.Text_Box_username.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+			this.Text_Box_username.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+			this.Text_Box_username.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+			this.Text_Box_username.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+			this.Text_Box_username.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+			this.Text_Box_username.Font = new System.Drawing.Font("Segoe UI", 9F);
+			this.Text_Box_username.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+			this.Text_Box_username.Location = new System.Drawing.Point(401, 169);
+			this.Text_Box_username.Name = "Text_Box_username";
+			this.Text_Box_username.PlaceholderText = "";
+			this.Text_Box_username.SelectedText = "";
+			this.Text_Box_username.Size = new System.Drawing.Size(229, 36);
+			this.Text_Box_username.TabIndex = 4;
+			this.Text_Box_username.TextChanged += new System.EventHandler(this.Text_Box_username_TextChanged);
+			// 
+			// guna2TextBox2
+			// 
+			this.guna2TextBox2.BorderRadius = 18;
+			this.guna2TextBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
+			this.guna2TextBox2.DefaultText = "";
+			this.guna2TextBox2.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+			this.guna2TextBox2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+			this.guna2TextBox2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+			this.guna2TextBox2.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+			this.guna2TextBox2.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+			this.guna2TextBox2.Font = new System.Drawing.Font("Segoe UI", 9F);
+			this.guna2TextBox2.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+			this.guna2TextBox2.Location = new System.Drawing.Point(401, 226);
+			this.guna2TextBox2.Name = "guna2TextBox2";
+			this.guna2TextBox2.PlaceholderText = "";
+			this.guna2TextBox2.SelectedText = "";
+			this.guna2TextBox2.Size = new System.Drawing.Size(229, 36);
+			this.guna2TextBox2.TabIndex = 5;
+			// 
+			// Button_login
+			// 
+			this.Button_login.BorderRadius = 18;
+			this.Button_login.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+			this.Button_login.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+			this.Button_login.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+			this.Button_login.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+			this.Button_login.FillColor = System.Drawing.Color.Turquoise;
+			this.Button_login.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Button_login.ForeColor = System.Drawing.Color.White;
+			this.Button_login.Location = new System.Drawing.Point(518, 298);
+			this.Button_login.Name = "Button_login";
+			this.Button_login.Size = new System.Drawing.Size(114, 36);
+			this.Button_login.TabIndex = 6;
+			this.Button_login.Text = "LOGIN";
+			this.Button_login.Click += new System.EventHandler(this.Button_login_Click);
+			// 
+			// label_clear
+			// 
+			this.label_clear.BorderRadius = 18;
+			this.label_clear.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+			this.label_clear.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+			this.label_clear.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+			this.label_clear.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+			this.label_clear.FillColor = System.Drawing.Color.Turquoise;
+			this.label_clear.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label_clear.ForeColor = System.Drawing.Color.White;
+			this.label_clear.Location = new System.Drawing.Point(403, 298);
+			this.label_clear.Name = "label_clear";
+			this.label_clear.Size = new System.Drawing.Size(114, 36);
+			this.label_clear.TabIndex = 7;
+			this.label_clear.Text = "CLEAR";
+			this.label_clear.Click += new System.EventHandler(this.label_clear_Click);
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.BackColor = System.Drawing.Color.Turquoise;
+			this.label4.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(42, 134);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(103, 30);
+			this.label4.TabIndex = 8;
+			this.label4.Text = "Welcome";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.BackColor = System.Drawing.Color.Turquoise;
+			this.label5.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(40, 214);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(121, 40);
+			this.label5.TabIndex = 9;
+			this.label5.Text = "AJWAH";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.BackColor = System.Drawing.Color.Turquoise;
+			this.label6.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new System.Drawing.Point(42, 261);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(130, 30);
+			this.label6.TabIndex = 10;
+			this.label6.Text = "Super_Shop";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.BackColor = System.Drawing.Color.Turquoise;
+			this.label7.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label7.Location = new System.Drawing.Point(42, 177);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(40, 30);
+			this.label7.TabIndex = 11;
+			this.label7.Text = "TO";
+			// 
+			// label_minimize
+			// 
+			this.label_minimize.AutoSize = true;
+			this.label_minimize.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label_minimize.Location = new System.Drawing.Point(608, 7);
+			this.label_minimize.Name = "label_minimize";
+			this.label_minimize.Size = new System.Drawing.Size(21, 30);
+			this.label_minimize.TabIndex = 13;
+			this.label_minimize.Text = "-";
+			this.label_minimize.Click += new System.EventHandler(this.label_minimize_Click);
+			this.label_minimize.MouseEnter += new System.EventHandler(this.label_minimize_MouseEnter);
+			this.label_minimize.MouseLeave += new System.EventHandler(this.label_minimize_MouseLeave);
+			// 
+			// label_exit
+			// 
+			this.label_exit.AutoSize = true;
+			this.label_exit.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label_exit.ForeColor = System.Drawing.Color.Black;
+			this.label_exit.Location = new System.Drawing.Point(632, 10);
+			this.label_exit.Name = "label_exit";
+			this.label_exit.Size = new System.Drawing.Size(24, 25);
+			this.label_exit.TabIndex = 14;
+			this.label_exit.Text = "X";
+			this.label_exit.Click += new System.EventHandler(this.label_exit_Click);
+			this.label_exit.MouseEnter += new System.EventHandler(this.label8_MouseEnter);
+			this.label_exit.MouseLeave += new System.EventHandler(this.label_exit_MouseLeave);
+			// 
+			// comboBox_role
+			// 
+			this.comboBox_role.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.comboBox_role.FormattingEnabled = true;
+			this.comboBox_role.Items.AddRange(new object[] {
+            "Admin",
+            "Seller"});
+			this.comboBox_role.Location = new System.Drawing.Point(417, 134);
+			this.comboBox_role.Name = "comboBox_role";
+			this.comboBox_role.Size = new System.Drawing.Size(197, 21);
+			this.comboBox_role.TabIndex = 15;
+			this.comboBox_role.Text = "Select_Role";
+			this.comboBox_role.SelectedIndexChanged += new System.EventHandler(this.comboBox_role_SelectedIndexChanged);
+			// 
+			// Loginform
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(668, 418);
+			this.Controls.Add(this.comboBox_role);
+			this.Controls.Add(this.label_exit);
+			this.Controls.Add(this.label_minimize);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label_clear);
+			this.Controls.Add(this.Button_login);
+			this.Controls.Add(this.guna2TextBox2);
+			this.Controls.Add(this.Text_Box_username);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.Label1);
+			this.Controls.Add(this.guna2CircleButton1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "Loginform";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButton1;
+		private System.Windows.Forms.Label Label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private Guna.UI2.WinForms.Guna2TextBox Text_Box_username;
+		private Guna.UI2.WinForms.Guna2TextBox guna2TextBox2;
+		private Guna.UI2.WinForms.Guna2Button Button_login;
+		private Guna.UI2.WinForms.Guna2Button label_clear;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label_minimize;
+		private System.Windows.Forms.Label label_exit;
+		private System.Windows.Forms.ComboBox comboBox_role;
+	}
+}
+
---
+++ Supermarket/Loginform.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Supermarket
+{
+	public partial class Loginform : Form
+	{
+		public Loginform()
+		{
+			InitializeComponent();
+			this.MouseDown += Loginform_MouseDown;
+		}
+		// to move the form freely in window.
+		[DllImport("user32.dll")]
+		public static extern bool ReleaseCapture();
+
+		[DllImport("user32.dll")]
+		public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+		public const int WM_NCLBUTTONDOWN = 0xA1;
+		public const int HTCAPTION = 0x2;
+
+		private void Loginform_MouseDown(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Left)
+			{
+				ReleaseCapture();
+				SendMessage(this.Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+			}
+		}
+
+		
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void guna2CircleButton1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label4_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label3_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Text_Box_username_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Button_login_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label_clear_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label_cross_Click(object sender, EventArgs e)
+		{
+
+		}
+		private void label8_MouseEnter(object sender, EventArgs e)
+		{
+			label_exit.ForeColor = Color.Red;
+		}
+
+		private void label_exit_MouseLeave(object sender, EventArgs e)
+		{
+			label_exit.ForeColor = Color.Black;
+		}
+
+		private void label_minimize_Click(object sender, EventArgs e)
+		{
+			this.WindowState = FormWindowState.Minimized;
+		}
+
+		private void label_exit_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		private void comboBox_role_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			comboBox_role.DropDownStyle = ComboBoxStyle.DropDownList; // disables typing
+			Text_Box_username.Focus();
+		}
+
+		private void label_minimize_MouseEnter(object sender, EventArgs e)
+		{
+			label_minimize.ForeColor = Color.Red;
+		}
+
+		private void label_minimize_MouseLeave(object sender, EventArgs e)
+		{
+			label_minimize.ForeColor = Color.Black;
+		}
+	}
+}
